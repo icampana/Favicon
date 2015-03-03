867df637855857a664caf6618739eea1bfc54a0e
Iván Campaña <ivan.campana@gmail.com>
2015-03-03 10:56:47 -0500

    Added the option to input the favicon path by hand instead of having to modify the plugin

--- Filter.php
+++ Filter.php
@@ -18,7 +18,16 @@ class Filter {
     public static function ipSendResponse($response)
     {
         if (is_object($response) && method_exists($response, 'setFavicon')) {
-            $response->setFavicon(ipFileUrl('Plugin/Favicon/assets/favicon.ico'));
+
+            $favicon = ipFileUrl('Plugin/Favicon/assets/favicon.ico');
+            $customFavicon = ipGetOption('Favicon.faviconPath');
+
+	    // Makes sure there is a favicon setup and is different from the default one
+            if ($customFavicon != '' and $customFavicon != 'favicon.ico'){
+                $favicon = ipFileUrl($customFavicon);
+            }
+
+            $response->setFavicon($favicon);
         }
         return $response;
     }
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # Favicon
 
-Please change favicon.ico file in assets directory.
+You can write an url to set the current favicon for the whole website or just change the favicon.ico file in assets directory.
